c5f87d8bf5a9703313527866ed4b16f14ba96f5e
Katarina <83969886+Katharsis15@users.noreply.github.com>
2021-05-30 21:59:22 +0300

    Add files via upload

--- load_photo.php
+++ load_photo.php
@@ -1,5 +1,35 @@
 <?php
 require 'database.php';
+
+if(isset($_FILES['uploadfile'])) {
+    $file = $_FILES['uploadfile'];
+    $errors = $_FILES['uploadfile']['error'];
+    $file_name = time() . "_" . $_FILES['uploadfile']['name'];
+    $file_size = $_FILES['uploadfile']['size'];
+    $file_tmp = $_FILES['uploadfile']['tmp_name'];
+    $file_type = $_FILES['uploadfile']['type'];
+    $file_ext = pathinfo($file_name, PATHINFO_EXTENSION);
+
+
+    if ($file_size <= 1073741824) {
+        if ($file_ext = 'jpg' or $file_ext = 'png' or $file_ext = 'jpeg') {
+            if ($errors == 0) {
+                move_uploaded_file($file_tmp, "images/" . $file_name);
+                $insert = mysqli_query($db, "INSERT INTO `files` (`path`) VALUES ('$file_name')");
+                echo 'Спасибо, что загрузили фото';
+            } else {
+                echo 'Ошибка';
+            }
+        } else {
+            echo 'Расширение файла не соответствует разрешённым расширениям: "jpg", "jpeg", "png"<br>';
+            echo 'Чтобы попробовать снова, нажмите' . ' ' . '<a href="photo.php">сюда</a>';
+        }
+    } else {
+        echo 'Размер файла превышает 1 ГБ <br>';
+        echo 'Чтобы попробовать снова, нажмите' . ' ' . '<a href="photo.php">сюда</a>';
+    }
+}
+/*************************************************
     /*$file = $_FILES['uploadfile'];//array(5) { ["name"]=> string(18) "rabbits.jpg" ["type"]=> string(10) "image/jpeg" ["tmp_name"]=> string(23) "C:\xampp\tmp\phpD09.tmp" ["error"]=> int(0) ["size"]=> int(679187) }
     $up = $_POST['upload'];//string(18) "Загрузить"
     $upl = $_FILES['uploadfile'];//array(5) { ["name"]=> string(18) "rabbits.jpg" ["type"]=> string(10) "image/jpeg" ["tmp_name"]=> string(24) "C:\xampp\tmp\php3F9C.tmp" ["error"]=> int(0) ["size"]=> int(679187) }
@@ -18,27 +48,30 @@ echo $path_parts['filename'], "\n";
 
 var_dump ($path_parts);*/
 ///////////////////
-      /*  if (isset($_POST['upload'])) {
+        /*if (isset($_POST['upload'])) {
         if (isset($_FILES['uploadfile'])) {
-            $photo_name = time() . "_" . basename($_FILES['uploadfile']['name']);
+            $name = time() . "_" . $_FILES['uploadfile']['name'];
+            $tmp_name = $_FILES['uploadfile']['tmp_name'];
             $error_flag = $_FILES['uploadfile']['error'];
 
             if ($error_flag == 0) {
-                $upfile = getcwd() . "/images/" . time() . "_" . basename(iconv('utf-8', 'windows-1251', $_FILES['uploadfile']['name']));
                 if ($_FILES['uploadfile']['size'] > 1024 * 1024 * 1024) {
                     echo 'Слишком большой размер файла (более 1 Гигабайта)<br>';
                     echo 'Чтобы попробовать снова, нажмите' . ' ' .'<a href="photo.php">сюда</a>';
                 }
                 if ($_FILES['uploadfile']['tmp_name']) {
-                    $allowed = array('jpg');
-                    $ext = pathinfo($_FILES['uploadfile'])['extension'];
-                        if (!in_array($ext, $allowed)) {
+                    //$allowed = array('jpg', 'jpeg', 'png', 'gif');
+                    move_uploaded_file($tmp_name, "images/" . $name);
+                    //pathinfo($_FILES['uploadfile'])['extension'];
+                    $path_info = pathinfo($_FILES['uploadfile']['name']);
+                    $ext = pathinfo($name, PATHINFO_EXTENSION);
+                        if ($ext != 'jpg') {
                             $errors[] = 'Неверный формат';
-                            echo 'Неверный формат<br>';
+                            echo 'Расширение файла не "jpg"<br>';
                             echo 'Чтобы попробовать снова, нажмите' . ' ' .'<a href="photo.php">сюда</a>';
-                        } else if (move_uploaded_file($_FILES['uploadfile']['tmp_name'], $upfile)) {
-                          $path = '/images/' . $photo_name;
-                            $insert = mysqli_query($db, "INSERT INTO `files` (`path`) VALUES '$path'");
+                        } else if (move_uploaded_file($tmp_name, "images/" . $name)) {
+                          $path = 'images/' . $name;
+                            $insert = mysqli_query($db, "INSERT INTO `files` (`path`) VALUES ('$name')");
                             echo 'Спасибо, что загрузили фото';
 
                         }
@@ -52,7 +85,10 @@ var_dump ($path_parts);*/
             $errors[] = 'Выберите изображение<br>';
             echo 'Чтобы попробовать снова, нажмите' . ' ' .'<a href="photo.php">сюда</a>';
         }
-    }
+    }*/
+
+
+
 
 /*$pa = pathinfo($file)['extension'];
 $exten = pathinfo($file)['dirname'];
@@ -94,10 +130,10 @@ function checkSize(float $size): bool
 //}*/
 
 //////////////////////////////////
-//$file = $_FILES['uploadfile'];
+/*$file = $_FILES['uploadfile'];
 $name = time() . "_" . $_FILES['uploadfile']['name'];
 $tmp_name = $_FILES['uploadfile']['tmp_name'];
 
 move_uploaded_file($tmp_name, "images/" . $name);
-$insert = mysqli_query($db, "INSERT INTO `files` (`path`) VALUES ('$name')");
+$insert = mysqli_query($db, "INSERT INTO `files` (`path`) VALUES ('$name')");*/
 //var_dump($name);
