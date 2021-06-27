583adb95000f4ae391ac697289aed73d49c4f095
Katarina <83969886+Katharsis15@users.noreply.github.com>
2021-06-27 17:56:41 +0300

    Add files via upload

--- make_order.php
+++ make_order.php
@@ -16,26 +16,37 @@ if (isset($_SESSION)) { ?>
 
     <p><b>Выберите, к какому мастеру Вы пойдёте:</b></p>
     <?php
-    $select_id = mysqli_query($db, "SELECT `id` FROM `users` WHERE `status` = 'master'");
-
-    $select_login = mysqli_query($db, "SELECT `login` FROM `users` WHERE `status` = 'master'");
-
-    $select_name = mysqli_query($db, "SELECT `name` FROM `users` WHERE `status` = 'master'");
-    $select_surname = mysqli_query($db, "SELECT `surname` FROM `users` WHERE `status` = 'master'");
-    var_dump($select_id);
+    //$select_login = mysqli_query($db, "SELECT `login` FROM `users` WHERE `status` = 'master'");
+    /*var_dump($select_id);
     var_dump($select_name);
     var_dump($select_surname);
     $ids_array = mysqli_fetch_assoc($select_id);
     $names_array = mysqli_fetch_assoc($select_name);
-    $surnames_array = mysqli_fetch_assoc($select_surname);
+    $surnames_array = mysqli_fetch_assoc($select_surname);*/
+
+     /*for ($i = 1; $i <= mysqli_num_rows(mysqli_query($db, "SELECT * FROM `users` WHERE `status` = 'master'")); $i++) {
+
+        $select_id = mysqli_query($db, "SELECT `id` FROM `users` WHERE `status` = 'master'");
+        $ids_array = mysqli_fetch_assoc($select_id);
+        $select_name = mysqli_query($db, "SELECT `name` FROM `users` WHERE `status` = 'master'");
+        $names_array = mysqli_fetch_assoc($select_name);
+        $select_surname = mysqli_query($db, "SELECT `surname` FROM `users` WHERE `status` = 'master'");
+        $surnames_array = mysqli_fetch_assoc($select_surname);
+
+        echo '<p><input name="master_id" type="radio" value="' .  $ids_array['id'] . '">' . $names_array['name'] . ' ' . $surnames_array['surname'] . '</p>';
+    }*/
 
 
-    $count_ids_array = array_count_values($ids_array);
-    var_dump($count_ids_array);
+    if($result = mysqli_query($db, "SELECT * FROM `users` WHERE `status` = 'master'")){
+        foreach($result as $row){
 
-/*foreach ( as ) {
+            $user_id = $row['id'];
+            $user_name = $row['name'];
+            $user_surname = $row['surname'];
+            echo '<p><input name="master_id" type="radio" value="' .  $user_id . '">' . $user_name . ' ' . $user_surname . '</p>';
+        }
+    }
 
-}*/
 /*$masters_array = mysqli_fetch_assoc($result);
 var_dump($masters_array);
 $master_id = $masters_array['id'];
